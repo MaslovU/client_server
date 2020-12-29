258f6f2a617f47b2bf5e96a59d4abcea35d0b6f1
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2020-12-29 18:07:14 +0500

    client_server homework_2

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,13 +1,37 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        Socket clientSocket = new Socket("localhost", 63368);
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        Socket clientSocket = new Socket("netology.homework", 63368);
 
-        out.println("Yury");
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+        out.println("");
         out.flush();
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String fromSrever = in.readLine();
+
+        if (fromSrever.equals("What is your name?")) {
+            out = new PrintWriter(clientSocket.getOutputStream());
+            out.println("Client Netology");
+            out.flush();
+        }
+
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        fromSrever = in.readLine();
+
+        if (fromSrever.equals("Are you child? (yes/no)")) {
+            out = new PrintWriter(clientSocket.getOutputStream());
+            out.println("yes");
+            out.flush();
+        }
+
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        fromSrever = in.readLine();
+        System.out.println(fromSrever);
     }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,10 +13,39 @@ public class Server {
         System.out.println("New connection accepted");
 
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
         final String name = in.readLine();
-        System.out.println("Gretting: " + name);
 
-        System.out.printf("Hello, %s, your port is %d%n", name, clientSocket.getPort());
+        if (name.isEmpty()) {
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+            out.println("What is your name?");
+            out.flush();
+        }
+
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String fromClient = in.readLine();
+        final String nameClient = fromClient;
+
+//        System.out.printf("Hello, %s, my port is %d%n", name, clientSocket.getLocalPort());
+
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+        out.println("Are you child? (yes/no)");
+        out.flush();
+
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        fromClient = in.readLine();
+
+        if (fromClient.equals("yes")) {
+            out = new PrintWriter(clientSocket.getOutputStream());
+            String toClient = String.format("Welcome to the kids area, %s! Let's play!", nameClient);
+            out.println(toClient);
+            out.flush();
+        }
+
+        if (fromClient.equals("no")) {
+            out = new PrintWriter(clientSocket.getOutputStream());
+            String toClient = String.format("Welcome to the adult zone, %s! Have a good rest, or a good working day!", nameClient);
+            out.println(toClient);
+            out.flush();
+        }
     }
 }
